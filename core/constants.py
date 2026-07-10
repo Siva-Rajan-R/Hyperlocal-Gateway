2df1c7c51325068c71317214b37e30d66d4a4b4d
Siva-Rajan-R <siva967763@gmail.com>
2026-07-10 16:46:33 +0530

    fixed

--- core/constants.py
+++ core/constants.py
@@ -1,6 +1,7 @@
 SERVICES={
     'utilities':'http://127.0.0.1:8000',
     'shops':'http://127.0.0.1:8001',
+    'auth':'http://127.0.0.1:8001',
     'employees':'http://127.0.0.1:8001',
     'suppliers':'http://127.0.0.1:8002',
     "purchases": 'http://127.0.0.1:8003',
@@ -8,5 +9,8 @@ SERVICES={
     "stockmovadj": 'http://127.0.0.1:8005',
     'customers':'http://127.0.0.1:8006' , 
     'orders':'http://127.0.0.1:8007',
-    'analytics':'http://127.0.0.1:8008'
+    'analytics':'http://127.0.0.1:8008',
+    'cart':'http://127.0.0.1:8007',
+    'analytics-dashboard':'http://127.0.0.1:8008',
+    'returns':'http://127.0.0.1:8007'
 }
